8a866fe9b41fbd211bf5d45635aa3de593d51483
Pavel <ctrlhakeem@yandex.ru>
2022-09-13 23:42:22 +0700

    added FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from 'react'
 import { Routes, Route, Link } from 'react-router-dom';
 import './styles/index.scss'
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,8 +16,8 @@ export const App = () => {
         <button onClick={toggleTheme}>toggle</button>
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                <Route path={'/about'} element={<AboutPageAsync />}/>
-                <Route path={'/'} element={<MainPageAsync />}/>
+                <Route path={'/about'} element={<AboutPage />}/>
+                <Route path={'/'} element={<MainPage />}/>
             </Routes>
         </Suspense>        
     </div>
