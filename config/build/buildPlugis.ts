6f35a6dd7763f1c18be6eefb161128011cc96081
Pavel <ctrlhakeem@yandex.ru>
2022-10-02 02:38:24 +0700

    fix plugins

--- config/build/buildPlugis.ts
+++ config/build/buildPlugis.ts
@@ -9,7 +9,7 @@ export function buildPlugins({
     isDev
 }: BuildOptions): WebpackPluginInstance[] {
 
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html
         }),
@@ -20,10 +20,15 @@ export function buildPlugins({
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
+        }),        
     ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
+    }
+
+    return plugins;
 }
