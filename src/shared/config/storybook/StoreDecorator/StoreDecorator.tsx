fe55af5a900c1617e9459e4e4221d4feca92d53d
Pavel <ctrlhakeem@yandex.ru>
2022-11-05 00:34:23 +0700

    fix storybook decorator

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,11 +1,24 @@
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
+
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+    loginForm: loginReducer,
+};
 
 // eslint-disable-next-line react/display-name
-export const StoreDecorator = (state: DeepPartial<StateSchema>) => (StoryComponent: Story) =>
+export const StoreDecorator =
     (
-        <StoreProvider initialState={state}>
-            <StoryComponent />
-        </StoreProvider>
-    );
+        state: DeepPartial<StateSchema>,
+        asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>,
+    ) =>
+    // eslint-disable-next-line react/display-name
+    (StoryComponent: Story) =>
+        (
+            <StoreProvider
+                initialState={state}
+                asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
+                <StoryComponent />
+            </StoreProvider>
+        );
