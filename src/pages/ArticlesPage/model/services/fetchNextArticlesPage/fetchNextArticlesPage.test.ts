dd805ffe1d62d69f45bc307b91bb7502feeee18c
Pavel <ctrlhakeem@yandex.ru>
2022-12-05 20:09:56 +0700

    production

--- src/pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage.test.ts
+++ src/pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage.test.ts
@@ -20,7 +20,7 @@ describe('fetchNextArticlesPage.test', () => {
         await thunk.callthunk();
 
         expect(thunk.dispatch).toBeCalledTimes(4);
-        expect(fetchArticlesList).toHaveBeenCalledWith({ page: 3 });
+        expect(fetchArticlesList).toHaveBeenCalledWith({});
     });
 
     test('fetchArticlesList not called', async () => {
