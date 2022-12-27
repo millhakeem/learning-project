f791357c58f7451bd9d48d89311687e6b02ce100
Pavel <ctrlhakeem@yandex.ru>
2022-12-27 17:20:33 +0700

    fix drawer, add browser list config

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -1,13 +1,12 @@
 import { NotificationList } from '@/entities/Notification';
-import { memo, useCallback, useState } from 'react';
-import { BrowserView, MobileView } from 'react-device-detect';
 import bellIcon from '@/shared/assets/icons/bell.svg';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { AnimationProvider } from '@/shared/lib/components/AnimationProvider';
 import { Button, ButtonTheme } from '@/shared/ui/Button/Button';
 import { Drawer } from '@/shared/ui/Drawer/Drawer';
 import { Icon } from '@/shared/ui/Icon/Icon';
 import { Popover } from '@/shared/ui/Popups';
+import { memo, useCallback, useState } from 'react';
+import { BrowserView, MobileView } from 'react-device-detect';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -47,11 +46,9 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
             </BrowserView>
             <MobileView>
                 {trigger}
-                <AnimationProvider>
-                    <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-                        <NotificationList />
-                    </Drawer>
-                </AnimationProvider>
+                <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+                    <NotificationList />
+                </Drawer>
             </MobileView>
         </div>
     );
