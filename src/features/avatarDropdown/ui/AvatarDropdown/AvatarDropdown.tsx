d94a14bcf5b3ffeab8ecd9b64ff6272151987449
Pavel <ctrlhakeem@yandex.ru>
2023-01-01 19:05:17 +0700

    add js doc

--- src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
@@ -1,17 +1,18 @@
 /* eslint-disable indent */
+import { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+
 import {
     getUserAuthData,
     isUserAdmin,
     isUserManager,
     userActions,
 } from '@/entities/User';
-import { memo, useCallback } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
+import { RoutePath } from '@/shared/const/router';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Avatar } from '@/shared/ui/Avatar';
 import { Dropdown } from '@/shared/ui/Popups';
-import { RoutePath } from '@/shared/const/router';
 
 interface AvatarDropdownProps {
     className?: string;
