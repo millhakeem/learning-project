aa7fb4ae727a67e74f0cf0b28c51a9c5a19eea31
Pavel <ctrlhakeem@yandex.ru>
2023-01-01 22:20:57 +0700

    upgrade webpack config, add babel cache, add postinstall script

--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -3,19 +3,20 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 export function buildCssLoader(isDev: boolean) {
     return {
         test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
         use: [
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
                 loader: 'css-loader',
                 options: {
                     modules: {
-                        auto: (resPath: string) => 
+                        auto: (resPath: string) =>
                             Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev 
-                            ? '[path][name]__[local]--[hash:base64:5]' 
-                            : '[hash:base64:8]'
-                    }                    
-                }
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
+                    },
+                },
             },
             'sass-loader',
         ],
---
+++ scripts/clear-cache.js
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const { join: joinPath } = require('path');
+
+const cacheDir = joinPath(__dirname, '..', 'node_modules/.cache');
+fs.rmSync(cacheDir, { recursive: true, force: true });
